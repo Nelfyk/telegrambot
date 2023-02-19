5fb4492f7e18c9376968b90768a6b709c0c470a0
Nelf <ruslan-burduzhan@mail.ru>
2023-02-19 14:34:26 +0700

    debugging

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 mvn clean package -DskipTests
 docker rm nelf_telegram_bot
 docker build -t nelf_telegram_bot .
-docker run --name nelf_telegram_bot --network=nelf-network -it -p 8080:8080 nelf_telegram_bot
+docker run --name nelf_telegram_bot --network=nelf-network -it -p 127.0.0.1:7776:8080 nelf_telegram_bot
